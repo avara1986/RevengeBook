5eba49a524ce96fa82ef673b4310f094d747c6ff
avara1986 <a.vara.1986@gmail.com>
2014-05-02 23:39:36 +0200

    Added conf mail vars to send mails

--- revengeBook/settings.py
+++ revengeBook/settings.py
@@ -108,3 +108,14 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.messages.context_processors.messages',
     'revengeapp.context_processors.get_revenge_points',
 )
+
+#MAIL. YOU MUST ADD YOUR CONNECTION
+#MAIL
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = ''
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
